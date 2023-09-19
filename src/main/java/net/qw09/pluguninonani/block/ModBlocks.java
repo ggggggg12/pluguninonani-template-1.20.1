5563bdf9e3294311c61657891649816a6ce9e58f
BuildTools <unconfigured@null.spigotmc.org>
2023-09-19 12:40:11 +0200

    important change

--- src/main/java/net/qw09/pluguninonani/block/ModBlocks.java
+++ src/main/java/net/qw09/pluguninonani/block/ModBlocks.java
@@ -1,24 +1,34 @@
 package net.qw09.pluguninonani.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.qw09.pluguninonani.item.ModItems;
 import net.qw09.pluguninonani.pluguninonani;
 
 public class ModBlocks {
 
     public static final Block DARKNESS_ORE = registerBlock("darkness_ore",
-            new Block(FabricBlockSettings.copy(Blocks.DEEPSLATE_DIAMOND_ORE).dropsLike()));
+            new Block(FabricBlockSettings.copy(Blocks.DEEPSLATE_DIAMOND_ORE)));
+
+    private static void addItemsToIngredientTab(FabricItemGroupEntries entries){
+        entries.add(DARKNESS_ORE);
+    }
+
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, new Identifier(pluguninonani.MOD_ID, name), block)
+        return Registry.register(Registries.BLOCK, new Identifier(pluguninonani.MOD_ID, name), block);
     }
 
     private static Item registerBlockItem(String name, Block block){
@@ -26,7 +36,10 @@ public class ModBlocks {
                 new BlockItem(block, new FabricItemSettings()));
     }
 
+
+
     public static void registerModlocks(){
         pluguninonani.LOGGER.info("registring mod blocks for" + pluguninonani.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(ModBlocks::addItemsToIngredientTab);
     }
 }
